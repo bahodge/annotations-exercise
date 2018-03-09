f0237643ec11d9f1ce64a1ec1a0f7ab4fc5e1072
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-03-09 12:19:46 -0600

    reverted back to broken exercise. Branches are still fixed though

--- src/Author.java
+++ src/Author.java
@@ -18,25 +18,25 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
-    @SuppressWarnings("unchecked")
+//    @SuppressWarnings("unchecked")
     @Deprecated
     public List<String> getBooks() {
         return books;
     }
 
-    @SuppressWarnings("unchecked")
+//    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
 
         return books;
     }
 
-    @SuppressWarnings("unchecked")
+//    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
 
-   @Override
-    public String fullName() {
+//   @Override
+    public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
 }
