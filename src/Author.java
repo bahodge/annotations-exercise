00e4b2ceb98bc1d656d4c6d5aeab258bcf3f2751
benjaminhodge <benjamin.andrew.hodge@gmail.com>
2018-03-09 10:09:45 -0600

    Added solution to get rid of warnings. Suppressed unchecked warnings

--- src/Author.java
+++ src/Author.java
@@ -4,8 +4,11 @@
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Author extends Person {
+
     private List books;
+
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
@@ -15,20 +18,24 @@ public class Author extends Person {
     /**
      * @deprecated Use publishedBooks instead
      */
+    @SuppressWarnings("unchecked")
     @Deprecated
     public List<String> getBooks() {
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public List<String> publishedBooks() {
+
         return books;
     }
 
+    @SuppressWarnings("unchecked")
     public void addBook(String book) {
         books.add(book);
     }
 
-    @Override
+   @Override
     public String fullName() {
         return String.format("%s, %s", lastName, firstName);
     }
